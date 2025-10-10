5a9bf8f07892f2a2a246054d58a317a1fa89fe9e
con4ig <s.szymon11@interia.pl>
2025-10-11 00:01:33 +0200

    feat(auth): add RBAC authorization middleware

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,6 @@ import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
-import router from './email.js';
 
 const app = express.Router();
 
