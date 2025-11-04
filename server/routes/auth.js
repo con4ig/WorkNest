9e968e9614aeea354033f864272155590c426c63
con4ig <s.szymon11@interia.pl>
2025-11-04 12:49:02 +0100

    test

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -46,7 +46,7 @@ router.post("/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 24 * 60 * 60 * 1000, // 24 godziny
     });
 
@@ -123,7 +123,7 @@ router.post("/logout", (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   });
 
   res.json({ message: "Wylogowano pomyślnie" });
