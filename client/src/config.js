4e044e81be34e0323a85687ca8f35742805cc98e
con4ig <s.szymon11@interia.pl>
2026-05-29 13:18:00 +0200

    chore(client): update application metadata and configuration references

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 export const siteConfig = {
-    administratorEmail: 's.szymon11@interia.pl',
-    administratorName: 'Szymon Wira',
+    administratorEmail: 's.con4ig11@interia.pl',
+    administratorName: 'con4ig',
     effectiveDate: '5.11.2025r.',
     siteUrl: 'https://worknest-1.onrender.com',
     companyName: 'WorkNest',
